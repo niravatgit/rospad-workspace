7681f73def8e56acb87a95e8cb4c3987145b6eaf
NIrav Patel <4724104+niravatgit@users.noreply.github.com>
2026-07-15 11:51:47 +0530

    Update src/demos/ur5_control/setup.py

---
+++ src/demos/ur5_control/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+package_name = 'ur5_control'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    entry_points={
+        'console_scripts': [
+            'joint_state_publisher = ur5_control.joint_state_publisher:main',
+            'joint_sine_demo       = ur5_control.joint_sine_demo:main',
+            'joint_controller      = ur5_control.joint_controller:main',
+        ],
+    },
+)
