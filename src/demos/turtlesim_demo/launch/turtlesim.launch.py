a3f2d56f03eabfae19aaad681b588ee557eaaedc
NIrav Patel <4724104+niravatgit@users.noreply.github.com>
2026-08-04 10:29:39 +0530

    Delete src/demos/turtlesim_demo/launch/turtlesim.launch.py

--- src/demos/turtlesim_demo/launch/turtlesim.launch.py
+++
@@ -1,9 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='turtlesim_demo', executable='turtle_node'),
-        Node(package='turtlesim_demo', executable='teleop_key'),
-    ])
