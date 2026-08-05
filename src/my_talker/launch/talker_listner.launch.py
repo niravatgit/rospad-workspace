f3ece3312254f8f1ccadea0880c4c3d897ece01a
NIrav Patel <4724104+niravatgit@users.noreply.github.com>
2026-08-05 18:16:49 +0530

    Update src/my_talker/launch/talker_listner.launch.py

--- src/my_talker/launch/talker_listner.launch.py
+++ src/my_talker/launch/talker_listner.launch.py
@@ -4,6 +4,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='my_talker', executable='my_node', name='talker'),
+        Node(package='my_talker', executable='my_talker', name='talker'),
         Node(package='my_talker', executable='listener', name='listener'),
     ])
