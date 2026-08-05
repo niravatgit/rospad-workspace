3037ec2929428f81e5d10781fd9305acca31cb01
NIrav Patel <4724104+niravatgit@users.noreply.github.com>
2026-08-05 18:28:15 +0530

    Update src/my_talker/setup.py

--- src/my_talker/setup.py
+++ src/my_talker/setup.py
@@ -10,7 +10,7 @@ setup(
     entry_points={
         'console_scripts': [ 
             'talker  = my_talker.talker:main',
-            'listener = my_talker.listner:main',
+            'listner = my_talker.listner:main',
             ],
     },
 )
