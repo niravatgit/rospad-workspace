6df8591d031fa7ae7823921b02971d25538bba61
NIrav Patel <4724104+niravatgit@users.noreply.github.com>
2026-08-06 15:34:05 +0530

    Delete src/my_talker/launch/talker_listner.launch.py

--- src/my_talker/launch/talker_listner.launch.py
+++
@@ -1,8 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='my_talker', executable='talker', name='talker'),
-        Node(package='my_talker', executable='listner', name='listner'),
-    ])
