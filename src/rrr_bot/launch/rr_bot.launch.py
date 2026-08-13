d3460220b67f6411916be9d4998ebebd2e78dfab
NIrav Patel <4724104+niravatgit@users.noreply.github.com>
2026-08-13 15:53:40 +0530

    Update src/rrr_bot/launch/rr_bot.launch.py

--- src/rrr_bot/launch/rr_bot.launch.py
+++ src/rrr_bot/launch/rr_bot.launch.py
@@ -20,7 +20,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     pkg = get_package_share_directory('rr_bot')
-    urdf_path = os.path.join(pkg, 'urdf', 'rr_.urdf')
+    urdf_path = os.path.join(pkg, 'urdf', 'rr_bot.urdf')
 
     with open(urdf_path, 'r') as f:
         robot_description = f.read()
